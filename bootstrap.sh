592a597f1ebb615c26110e89078a80228d1905a8
bleed4glory <bleed4glory@gmail.com>
2013-07-08 10:56:27 -0700

    redid configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,18 @@
 apt-get update
 
 #install some pre-reqs
-apt-get install -y git curl build-essential libssl-dev libcurl4-openssl-dev libsqlite3-dev
+apt-get install -y git nodejs curl build-essential libssl-dev libcurl4-openssl-dev libsqlite3-dev zlib1g-dev libpcre3-dev libgeoip-dev checkinstall
 
 #install rbenv, ruby, rails and bundler 
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL -l
+#exec $SHELL -l
 mkdir -p ~/.rbenv/plugins
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 git clone git://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
-rbenv install 2.0.0-p195
-rbenv global 2.0.0-p195
+$HOME/.rbenv/bin/rbenv install 2.0.0-p195
+$HOME/.rbenv/bin/rbenv global 2.0.0-p195
 gem install bundler
 gem install rails --no-rdoc --no-ri
 
@@ -27,15 +27,16 @@ cd /usr/local
 rails new helloworld
 
 #configure unicorn
+mkdir helloworld/tmp/pids
 cd helloworld/config
-wget https://raw.github.com/bleed4glory/cic/master/unicorn.rb config
+wget https://raw.github.com/bleed4glory/cic/master/unicorn.rb
 
 mkdir /etc/unicorn
 cd /etc/unicorn
 wget https://raw.github.com/bleed4glory/cic/master/helloworld.conf
 
-
-wget https://raw.github.com/bleed4glory/cic/master/unicorn_init /etc/init.d/unicorn_init.sh
+cd /etc/init.d
+wget https://raw.github.com/bleed4glory/cic/master/unicorn_init
 
 #set permissions on the script
 chmod 755 /etc/init.d/unicorn_init
