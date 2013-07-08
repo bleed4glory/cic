01ded5353a43b21c382a0069f23e0200d09a1797
bleed4glory <bleed4glory@gmail.com>
2013-07-08 13:17:35 -0700

    k

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,27 +6,32 @@ apt-get update
 #install some pre-reqs
 apt-get install -y git nodejs curl build-essential libssl-dev libcurl4-openssl-dev libsqlite3-dev zlib1g-dev libpcre3-dev libgeoip-dev checkinstall
 
-#install rbenv, ruby, rails and bundler 
-#git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
-#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-#echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+echo "install rbenv & ruby"
+sleep 2
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 #exec $SHELL -l
-#mkdir -p ~/.rbenv/plugins
-#git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-#git clone git://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
-#~/.rbenv/bin/rbenv install 2.0.0-p195
-#~/.rbenv/bin/rbenv global 2.0.0-p195
-#~/.rbenv/bin/rbenv rehash
+source ~./bashrc
+mkdir -p ~/.rbenv/plugins
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone git://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+~/.rbenv/bin/rbenv install 2.0.0-p195
+~/.rbenv/bin/rbenv global 2.0.0-p195
+~/.rbenv/bin/rbenv rehash
+ruby -v
 
 #install ruby from source
-wget ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p247.tar.gz
+#wget ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p247.tar.gz
 
-tar -xvf ruby-2.0.0-p247.tar.gz
+#tar -xvf ruby-2.0.0-p247.tar.gz
+#cd ruby-2.0.0-p247
+#./configure  
+#make
+#make install
 
-./configure  
-make
-make install
-
+echo "install rails and bundler"
+sleep 2
 gem install rails --no-ri --no-rdoc
 gem install bundler --no-ri --no-rdoc
 
